2857916b5f3a5f01b66908efa77d67d7ce425caf
fathimasilviyak <silviyakvm@gmail.com>
2023-04-02 17:22:17 +0530

    convert a series to a list

--- main.py
+++ main.py
@@ -20,3 +20,7 @@ data = pandas.read_csv("weather_data.csv")
 # print(type(data["temp"]))
 data_dict = data.to_dict()
 print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+print(len(temp_list))

408a216a3c3fcf510c3c109ba8ef5704a8bd7c51
fathimasilviyak <silviyakvm@gmail.com>
2023-04-02 16:52:23 +0530

    read data using pandas

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data:
-        print(row)
-        if row[1] != "temp":
-            temperature.append(int(row[1]))
-print(temperature)
+# import csv
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         print(row)
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
 
+import pandas
+data = pandas.read_csv("weather_data.csv")
+print(data)

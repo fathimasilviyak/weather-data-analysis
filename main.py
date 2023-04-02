8ae8fd6e5420abb207ff7b8ad27435b1d748f756
fathimasilviyak <silviyakvm@gmail.com>
2023-04-02 17:27:54 +0530

    get average temperature

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ data = pandas.read_csv("weather_data.csv")
 # print(type(data))
 # print(type(data["temp"]))
 data_dict = data.to_dict()
-print(data_dict)
+# print(data_dict)
 
 temp_list = data["temp"].to_list()
-print(temp_list)
-print(len(temp_list))
+# print(temp_list)
+# print(len(temp_list))
+
+print(data["temp"].mean())
